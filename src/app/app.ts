3c933414d876b1771bef6d8055574bf6dcc11b84
Richel Norland <richel265@tuta.io>
2025-11-19 22:57:09 -0500

    Add Nav and Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { Nav } from "./components/nav/nav";
+import { Main } from "./components/main/main";
+import { Footer } from "./components/footer/footer";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  selector: "app-root",
+  imports: [Nav, Main, Footer],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App {
-  protected readonly title = signal('sxlphuric-website');
+  protected readonly title = signal("sxlphuric-website");
 }
